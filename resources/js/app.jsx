954bc0dd42d9aa2465f7a354c2f8b91f59dd215a
mokadde <mokaddes.ru2000@gmail.com>
2024-10-11 09:10:05 +0600

    commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/about';
+
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+<About />
+);
